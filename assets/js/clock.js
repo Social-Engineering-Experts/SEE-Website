129a125d5e54770f6cdda7571e9c6f20af07a357
Zhang Zeyu <zhangzeyu2001@gmail.com>
2022-04-25 13:22:21 +0800

    Fix negative clock after CTF started

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -2,7 +2,7 @@ const startTime = new Date(Date.parse("04 Jun 2022 00:00:00 GMT+0800"));
 const endTime = new Date(Date.parse("06 Jun 2022 00:00:00 GMT+0800"))
 
 const getTimeRemaining = (endtime) => {
-  let t = Date.parse(endtime) - Date.parse(new Date());
+  let t = Math.max(Date.parse(endtime) - Date.parse(new Date()), 0);
   let seconds = Math.floor((t / 1000) % 60);
   let minutes = Math.floor((t / 1000 / 60) % 60);
   let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
@@ -28,12 +28,9 @@ const initializeClock = (id, endtime) => {
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
   }
   updateClock();
-  let timeinterval = setInterval(updateClock, 1000);
+  setInterval(updateClock, 1000);
 }
 
 initializeClock('clockdiv', startTime);

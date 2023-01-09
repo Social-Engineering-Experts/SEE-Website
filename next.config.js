50e1cf7b173a157c80bfd980d9aca878e3d2d32a
Sebastian <syii@Sebastians-MacBook-Pro.local>
2023-01-09 10:34:16 +0800

    Modifiy Next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath,
-  assetPrefix,
+
 }
 
 module.exports = nextConfig;

e40054ae78043e556ad0f7bfc0a3482ef085271b
Sebastian <syii@Sebastians-MacBook-Pro.local>
2023-01-09 10:08:03 +0800

    Workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath,
+  assetPrefix,
 }
 
 module.exports = nextConfig;
